9a05e609917ec8c81a31d43e9f88a962739e985d
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-11 18:06:02 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -297,8 +297,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('More Bots', url='https://t.me/subin_works/122'),
-                        InlineKeyboardButton('Update Channel', url='https://t.me/subin_works')
+                        InlineKeyboardButton('More Bots', url='https://t.me/tg_bots_updates'),
+                        InlineKeyboardButton('Update Channel', url='https://t.me/tg_bots_updates')
                     ]
                     ]
                 
