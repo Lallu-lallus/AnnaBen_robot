bff6768c75f7f91e2317c1057051f0e804c9f3c8
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-12 16:01:58 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -129,7 +129,7 @@ async def group(client, message):
         if not btn:
             return
 
-            if len(btn) > 10: 
+        if len(btn) > 10: 
             btns = list(split_list(btn, 10)) 
             keyword = f"{message.chat.id}-{message.message_id}"
             BUTTONS[keyword] = {
