bfd53bfe95f3b3ede31f558633c0ba41d951e1e3
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-24 20:08:51 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,51 +1,22 @@
-@cilent.on_message(filters.new_chat_members)
-async def auto_welcome(bot: Pr0fess0r_99, msg: Message):
-    # from PR0FESS0R-99 import ID-Bot
-    first = msg.from_user.first_name
-    last = msg.from_user.last_name
-    mention = msg.from_user.mention
-    username = msg.from_user.username
-    id = msg.from_user.id
-    group_name = msg.chat.title
-    group_username = msg.chat.username
-    name_button = "🔰 JOIN NOW 🔰"
-    link_button = "t.me/Mo_tech_YT"
-    button_name = os.environ.get("WELCOME_BUTTON_NAME", name_button)
-    button_link = os.environ.get("WELCOME_BUTTON_LINK", link_button)
-    welcome_text = f"Hey {mention}\nWelcome To {group_name}"
-    WELCOME_TEXT = os.environ.get("WELCOME_TEXT", welcome_text)
-    print("Welcome Message Activate")
-    BUTTON = bool(os.environ.get("WELCOME_BUTTON"))
-    if not BUTTON:
-       await msg.reply_text(text=WELCOME_TEXT.format(
-           first = msg.from_user.first_name,
-           last = msg.from_user.last_name,
-           username = None if not msg.from_user.username else '@' + msg.from_user.username,
-           mention = msg.from_user.mention,
-           id = msg.from_user.id,
-           group_name = msg.chat.title,
-           group_username = None if not msg.chat.username else '@' + msg.chat.username
-          )
-       )
-    else:
-       await msg.reply_text(text=WELCOME_TEXT.format(
-           first = msg.from_user.first_name,
-           last = msg.from_user.last_name,
-           username = None if not msg.from_user.username else '@' + msg.from_user.username,
-           mention = msg.from_user.mention,
-           id = msg.from_user.id,
-           group_name = msg.chat.title,
-           group_username = None if not msg.chat.username else '@' + msg.chat.username
-          ),
-       reply_markup=InlineKeyboardMarkup(
-               [
-                   [
-                       InlineKeyboardButton
-                           (
-                               button_name, url=button_link
-                           )
-                   ]  
-               ]
-           )
-       )  
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message, User
 
+bughunter0 = Client(
+    "BotNameHere",
+     bot_token = os.environ["BOT_TOKEN"],
+     api_id = int(os.environ["API_ID"]),
+     api_hash = os.environ["API_HASH"]
+)
+
+@bughunter0.on_message(filters.new_chat_members)
+async def welcome(bot,message):
+	chatid= message.chat.id
+	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+	
+@bughunter0.on_message(filters.left_chat_member)
+async def goodbye(bot,message):
+	chatid= message.chat.id
+	await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
+	
+bughunter0.run()
