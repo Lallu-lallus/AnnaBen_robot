7ba4b7d67862fd343740c7cc373d95b804648ab2
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-24 20:45:57 +0530

    Create Main.py

---
+++ Alpha_Botz/Main.py
@@ -0,0 +1,22 @@
+import os
+from pyrogram import Client, filters
+from pyrogram.types import Message, User
+
+bughunter0 = Client(
+    "BotNameHere",
+     bot_token = os.environ["BOT_TOKEN"],
+     api_id = int(os.environ["API_ID"]),
+     api_hash = os.environ["API_HASH"]
+)
+
+@bughunter0.on_message(filters.new_chat_members)
+async def welcome(bot,message):
+	chatid= message.chat.id
+	await bot.send_message(text=f"Welcome {message.from_user.mention} to {message.chat.username} ,  Happy to have here",chat_id=chatid)
+	
+@bughunter0.on_message(filters.left_chat_member)
+async def goodbye(bot,message):
+	chatid= message.chat.id
+	await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
+	
+bughunter0.run()
