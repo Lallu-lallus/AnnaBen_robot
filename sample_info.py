84d749d78a9faf0d64303e532cb7cef3b4fd1510
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-24 16:53:10 +0530

    Update sample_info.py

--- sample_info.py
+++ sample_info.py
@@ -23,9 +23,14 @@ COLLECTION_NAME = 'channel_files'  # If you are using the same database, then us
 
 # Messages
 START_MSG = """
-**Hi, I'm Media Search bot**
+**👋Hey{msg.from_user.mention}, 
+𝙼𝚈 𝙽𝙰𝙼𝙴 Anna_ben{tg://settings} 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂 𝙸𝙽 𝙶𝚁𝙾𝚄𝙿𝚂,**
 
-Here you can search files in inline mode. Just press follwing buttons and start searching.
+𝙸𝚃'𝚂 𝚅𝙴𝚁𝚈 𝙴𝙰𝚂𝚈. 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙼𝙰𝙺𝙴 𝙼𝙴 𝙰𝙳𝙼𝙸𝙽, 𝚃𝙷𝙰𝚃𝚂 𝙰𝙻𝙻, 𝙸'𝙻𝙻 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂 𝚃𝙷𝙴𝚁𝙴 🤓
+
+
+➖➖➖➖➖➖➖➖➖➖➖➖
+©️MᴀɪɴᴛᴀɪɴᴇD Bʏ :a href="https://t.me/Pro_editor_tg"> Lallu-llalus</a>
 """
 
 SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
