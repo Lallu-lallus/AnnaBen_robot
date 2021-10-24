3428e83bf037c4ed7647d02e4d22f55dd2f6b600
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-24 12:32:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -379,4 +379,4 @@ async def cb_handler(client: Client, query: CallbackQuery):
         elif query.data == "pages":
             await query.answer()
     else:
-        await query.answer("കൌതുകും ലേശം കൂടുതൽ ആണല്ലേ👀",show_alert=True)
+        await query.answer("കൌതുകും ലേശം കൂടുതൽ ആണല്ലേ😌👀",show_alert=True)
