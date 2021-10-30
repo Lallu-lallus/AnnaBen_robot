d2249104d27cc2a2b2baf3a4b0046acb0d95b3d3
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-10-30 12:39:39 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -2,22 +2,7 @@ import re
 import base64
 import logging
 from struct import pack
-def get_file_id(msg: Message):
-    if msg.media:
-        for message_type in (
-            "photo",
-            "animation",
-            "audio",
-            "document",
-            "video",
-            "video_note",
-            "voice",
-            "sticker"
-        ):
-            obj = getattr(msg, message_type)
-            if obj:
-                setattr(obj, "message_type", message_type)
-                return obj
+from pyrogram.types import Message
 from pyrogram.errors import UserNotParticipant
 from pyrogram.file_id import FileId
 from pyrogram.types import Message
