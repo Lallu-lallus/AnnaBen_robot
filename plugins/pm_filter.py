bed273c8c98803e57ec56bc4bc516eeb83938d71
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-18 18:24:07 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -471,7 +471,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
        ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await query.message.edit_text(
-            text=script.START_TXT.format(temp.B_NAME),
+            text=script.FILTER_TXT.format(temp.B_NAME),
             reply_markup=reply_markup,
             parse_mode='html'
         )
