fb39b563cd8e7445496d86dfaac90839e173b105
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-27 15:57:53 +0530

    Create luck.py

---
+++ plugins/admemes/luck.py
@@ -0,0 +1,26 @@
+
+
+from pyrogram import Client, filters
+from info import COMMAND_HAND_LER
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
+
+# EMOJI CONSTANTS
+TRY_YOUR_LUCK = "🎰"
+# EMOJI CONSTANTS
+
+
+@Client.on_message(
+    filters.command(["luck", "cownd"], COMMAND_HAND_LER) &
+    f_onw_fliter
+)
+async def luck_cound(client, message):
+    """ @luck """
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_luck(
+        chat_id=message.chat.id,
+        emoji=TRY_YOUR_LUCK,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
