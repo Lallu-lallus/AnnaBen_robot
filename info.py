a5ee63d26c36577496851260ee38a9f864840a3e
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-28 18:13:23 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,6 @@ AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 # Command
 COMMAND_HAND_LER = environ.get("COMMAND_HAND_LER", "/")
 
-
 # MongoDB information
 DATABASE_URI = environ.get('DATABASE_URI', "")
 DATABASE_NAME = environ.get('DATABASE_NAME', "tgmoviebot")
