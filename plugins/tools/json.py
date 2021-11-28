9db0cd8a3b0e82173da089a07ed622adee9d7d3b
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-28 15:03:48 +0530

    Update json.py

--- plugins/tools/json.py
+++ plugins/tools/json.py
@@ -1,13 +1,9 @@
-"""Get Detailed info about any message
-Syntax: .json"""
 
 import os
 from pyrogram import Client, filters
-from info import COMMAND_HAND_LER
-from plugins.helper_functions.cust_p_filters import sudo_filter
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message, CallbackQuery
 
-
-@Client.on_message(filters.command("json", COMMAND_HAND_LER) & sudo_filter)
+@Client.on_message(filters.command(["json", 'js', 'showjson']))
 async def jsonify(_, message):
     the_real_message = None
     reply_to_id = None
@@ -16,16 +12,36 @@ async def jsonify(_, message):
         the_real_message = message.reply_to_message
     else:
         the_real_message = message
-
     try:
-        await message.reply_text(f"<code>{the_real_message}</code>")
+        pk = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="✗ close ✗",
+                        callback_data="close_data"
+                    )
+                ]
+            ]
+        )
+        await message.reply_text(f"<code>{the_real_message}</code>", reply_markup=pk, quote=True)
     except Exception as e:
         with open("json.text", "w+", encoding="utf8") as out_file:
             out_file.write(str(the_real_message))
+        reply_markup = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="✗ close ✗",
+                        callback_data="close_data"
+                    )
+                ]
+            ]
+        )
         await message.reply_document(
             document="json.text",
             caption=str(e),
             disable_notification=True,
-            reply_to_message_id=reply_to_id
-        )
+            quote=True,
+            reply_markup=reply_markup
+        )            
         os.remove("json.text")
