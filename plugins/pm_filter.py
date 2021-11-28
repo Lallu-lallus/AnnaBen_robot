27880089123f941e3314f3d9ce224a512900be3a
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-28 17:03:20 +0530

    🤡

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -559,6 +559,16 @@ async def cb_handler(client: Client, query: CallbackQuery):
             reply_markup=reply_markup,
             parse_mode='html'
         )
+    elif query.data == "fun":
+        buttons = [[
+            InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='filter')
+        ]]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        await query.message.edit_text(
+            text=script.FUN_TXT,
+            reply_markup=reply_markup,
+            parse_mode='html'
+        )         
     elif query.data == "coct":
         buttons = [[
             InlineKeyboardButton('« Back', callback_data='help')
