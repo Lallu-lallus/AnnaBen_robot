41b37ef5316ae159a532e2838f9473ee604f1c31
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-28 15:44:32 +0530

    😂🤧

--- utils.py
+++ utils.py
@@ -1,6 +1,6 @@
 import logging
 from pyrogram.errors import InputUserDeactivated, UserNotParticipant, FloodWait, UserIsBlocked, PeerIdInvalid
-from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION
+from info import AUTH_CHANNEL, LONG_IMDB_DESCRIPTION, MAX_LIST_ELM
 from imdb import IMDb
 import asyncio
 from pyrogram.types import Message
@@ -11,6 +11,8 @@ from datetime import datetime
 from typing import List
 from pyrogram.types import InlineKeyboardButton
 from database.users_chats_db import db
+from bs4 import BeautifulSoup
+import requests
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
@@ -28,21 +30,13 @@ START_CHAR = ('\'', '"', SMART_OPEN)
 
 # temp db for banned 
 class temp(object):
-
     BANNED_USERS = []
-
     BANNED_CHATS = []
-
     ME = None
-
     CURRENT=int(os.environ.get("SKIP", 2))
-
     CANCEL = False
-
     MELCOW = {}
-
     U_NAME = None
-
     B_NAME = None
 
 async def is_subscribed(bot, query):
@@ -58,17 +52,21 @@ async def is_subscribed(bot, query):
 
     return False
 
-async def get_poster(query, bulk=False, id=False):
+async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
         # https://t.me/GetTGLink/4183
-        pattern = re.compile(r"^(([a-zA-Z\s])*)?\s?([1-2]\d\d\d)?", re.IGNORECASE)
-        match = pattern.match(query)
-        year = None
-        if match:
-            title = match.group(1)
-            year = match.group(3)
+        query = (query.strip()).lower()
+        title = query
+        year = re.findall(r'[1-2]\d{3}$', query, re.IGNORECASE)
+        if year:
+            year = list_to_str(year[:1])
+            title = (query.replace(year, "")).strip()
+        elif file is not None:
+            year = re.findall(r'[1-2]\d{3}', file, re.IGNORECASE)
+            if year:
+                year = list_to_str(year[:1]) 
         else:
-            title = query
+            year = None
         movieid = imdb.search_movie(title.lower(), results=10)
         if not movieid:
             return None
@@ -87,9 +85,6 @@ async def get_poster(query, bulk=False, id=False):
     else:
         movieid = int(query)
     movie = imdb.get_movie(movieid)
-    title = movie.get('title')
-    genres = ", ".join(movie.get("genres")) if movie.get("genres") else None
-    rating = str(movie.get("rating"))
     if movie.get("original air date"):
         date = movie["original air date"]
     elif movie.get("year"):
@@ -97,7 +92,7 @@ async def get_poster(query, bulk=False, id=False):
     else:
         date = "N/A"
     plot = ""
-    if LONG_IMDB_DESCRIPTION:
+    if not LONG_IMDB_DESCRIPTION:
         plot = movie.get('plot')
         if plot and len(plot) > 0:
             plot = plot[0]
@@ -135,7 +130,6 @@ async def get_poster(query, bulk=False, id=False):
         'rating': str(movie.get("rating")),
         'url':f'https://www.imdb.com/title/tt{movieid}'
     }
-
 # https://github.com/odysseusmax/animated-lamp/blob/2ef4730eb2b5f0596ed6d03e7b05243d93e3415b/bot/utils/broadcast.py#L37
 
 async def broadcast_messages(user_id, message):
@@ -158,6 +152,22 @@ async def broadcast_messages(user_id, message):
         return False, "Error"
     except Exception as e:
         return False, "Error"
+
+async def search_gagala(text):
+    usr_agent = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+        'Chrome/61.0.3163.100 Safari/537.36'
+        }
+    text = text.replace(" ", '+')
+    url = f'https://www.google.com/search?q={text}'
+    response = requests.get(url, headers=usr_agent)
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+    titles = soup.find_all( 'h3' )
+    return [title.getText() for title in titles]
+
+
+
 
 def get_size(size):
     """Get size in readable format"""
@@ -227,8 +237,12 @@ def list_to_str(k):
         return "N/A"
     elif len(k) == 1:
         return str(k[0])
+    elif MAX_LIST_ELM:
+        k = k[:int(MAX_LIST_ELM)]
+        return ' '.join(f'{elem}, ' for elem in k)
     else:
         return ' '.join(f'{elem}, ' for elem in k)
+
 def last_online(from_user):
     time = ""
     if from_user.is_bot:
