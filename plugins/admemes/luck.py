e8ab48904e5d930a5e01da962a416e2fbea4b8c4
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-30 15:11:05 +0530

    🙂

--- plugins/admemes/luck.py
+++ plugins/admemes/luck.py
@@ -4,21 +4,21 @@ from pyrogram import Client, filters
 from info import COMMAND_HAND_LER
 from plugins.helper_functions.cust_p_filters import f_onw_fliter
 
+# LUCK------------ https://telegram.me/Josprojects ------------ #
+
 # EMOJI CONSTANTS
 TRY_YOUR_LUCK = "🎰"
 # EMOJI CONSTANTS
 
-
 @Client.on_message(
-    filters.command(["luck", "cownd"], COMMAND_HAND_LER) &
-    f_onw_fliter
+    filters.command(["luck", "cownd"])
 )
-async def luck_cound(client, message):
+async def luck_cownd(client, message):
     """ /luck an @animatedluck """
     rep_mesg_id = message.message_id
     if message.reply_to_message:
         rep_mesg_id = message.reply_to_message.message_id
-    await client.send_luck(
+    await client.send_dice(
         chat_id=message.chat.id,
         emoji=TRY_YOUR_LUCK,
         disable_notification=True,
