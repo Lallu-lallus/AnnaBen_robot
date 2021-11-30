ae0dfdbee073f8e29d56290905a17acb9a74b6e4
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-11-30 15:14:41 +0530

    🤔

--- Script.py
+++ Script.py
@@ -67,7 +67,8 @@ t𝗋𝗒 𝗍𝗁𝗂𝗌 𝖮𝗎𝗍:
 𝟣. /dice - Roll The Dice 
 𝟤. /Throw 𝗈𝗋 /Dart - 𝖳𝗈 𝖬𝖺𝗄𝖾 Drat 
 3. /Runs - Jokes 
-4. /Goal or /Shoot - To Make A Goal Or Shoot"""
+4. /Goal or /Shoot - To Make A Goal Or Shoot
+5. /luck or /cownd - Spin the Lucky"""
     MANUALFILTER_TXT = """Help: <b>Filters</b>
 
 - Filter is the feature were users can set automated replies for a particular keyword and Dingdi will respond whenever a keyword is found the message
