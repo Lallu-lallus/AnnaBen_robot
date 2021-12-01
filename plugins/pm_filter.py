723979b314f075650ec6bb66a3f09f27eaff92e5
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-01 18:46:49 +0530

    🤧

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -516,6 +516,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             text=script.ALIVE_TXT,
             reply_markup=reply_markup,
             parse_mode='html'
+        )
     elif query.data == "whois":
         buttons = [[
             InlineKeyboardButton('ʙᴀᴄᴋ', callback_data='help')
