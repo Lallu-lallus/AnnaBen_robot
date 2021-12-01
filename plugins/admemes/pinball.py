32dd00695d2c0bb40177e61a9ab4c8d7ac49c1b8
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-01 20:18:08 +0530

    🎳

---
+++ plugins/admemes/pinball.py
@@ -0,0 +1,35 @@
+from pyrogram import Client, filters
+from info import COMMAND_HAND_LER
+from plugins.helper_functions.cust_p_filters import f_onw_fliter
+
+# LUCK------------ https://telegram.me/Josprojects ------------ #
+
+# EMOJI CONSTANTS
+PIN_BALL = "🎳"
+# EMOJI CONSTANTS
+
+@Client.on_message(
+    filters.command(["pinball", "tenpin"])
+)
+async def pinball_tenpin(client, message):
+    """ /pinball an @animatedpinball """
+    rep_mesg_id = message.message_id
+    if message.reply_to_message:
+        rep_mesg_id = message.reply_to_message.message_id
+    await client.send_dice(
+        chat_id=message.chat.id,
+        emoji=PIN_BALL,
+        disable_notification=True,
+        reply_to_message_id=rep_mesg_id
+    )
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
