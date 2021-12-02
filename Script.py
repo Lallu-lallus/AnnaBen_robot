4f2142b14615939e37462a6ee0c74d3822865e3f
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-02 20:33:56 +0530

    🤧🤕

--- Script.py
+++ Script.py
@@ -35,6 +35,7 @@ Just for a rasam😂"""
 ➡️ /covid <code>(countryname)</code> <b>you can find a corona information of every country</b>
 
 ➡️ <b>example</b> : - /covid India"""
+    STICKER_TEXT ="""<b>COMMAND /stickerid\n𝖨𝖿 𝖸𝗈𝗎 𝖭𝖾𝖾𝖽 𝖳𝖾𝗅𝖾𝗀𝗋𝖺𝗆 𝖲𝗍𝗂𝖼𝗄𝖾𝗋 𝖨𝖽 𝖢𝗅𝗂𝖼𝗄 /stickerid 𝖳𝗈 𝖦𝖾𝗍 𝖲𝗍𝗂𝖼𝗄𝖾𝗋 𝖨𝖽 (𝖱𝖾𝗉𝗅𝗒 𝖶𝗂𝗍𝗁 𝖲𝗍𝗂𝖼𝗄𝖾𝗋)</b>"""
     SONG_TXT ="""<b>SONG MODULE</b>
 Song Download
 Song Download Module, For Those Who Love Music
