14b2862142bdb8ed460468f6fba97e779a4c8d5b
SANJU <89297664+LolanBot18@users.noreply.github.com>
2021-12-02 19:35:08 +0530

    Update corona.py

--- plugins/admemes/corona.py
+++ plugins/admemes/corona.py
@@ -7,7 +7,7 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 API = "https://api.sumanjay.cf/covid/?country="
 
-BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join our Channel ⚙', url='https://telegram.me/cz_films')]])
+BUTTONS = InlineKeyboardMarkup([[InlineKeyboardButton('⚙ Join our Channel ⚙', url='https://t.me/team_annaben')]])
 
 @Client.on_message(filters.command("covid"))
 async def reply_info(client, message):
