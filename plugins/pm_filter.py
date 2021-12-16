5f1a8df79691fa078e77ebf8fd1f92e2fc5f07f8
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2021-12-16 11:59:01 +0530

    🤕

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -697,10 +697,8 @@ InlineKeyboardButton('manual', callback_data='manualfilter')
             InlineKeyboardButton('« Back', callback_data='help')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
-
-await query.message.edit_text(
-
-text=script.ZOMBIES_TXT,
+        await query.message.edit_text(
+            text=script.ZOMBIES_TXT,
             disable_web_page_preview=True,
             reply_markup=reply_markup,
             parse_mode='html'
