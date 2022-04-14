29284c93d05ff24ab7c4a7f95de1b4acd95124e1
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2022-04-14 23:43:33 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -427,7 +427,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
         f_caption=files.caption
         if CUSTOM_FILE_CAPTION:
             try:
-                f_caption=CUSTOM_FILE_CAPTION.format(file_name=title, file_size=size, file_caption=f_caption)
+                f_caption=CUSTOM_FILE_CAPTION.format(m = query.from_user.mention,lallus = time,file_name=title, file_size=size, file_caption=f_caption)
             except Exception as e:
                 logger.exception(e)
                 f_caption=f_caption
