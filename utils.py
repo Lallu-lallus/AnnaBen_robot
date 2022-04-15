2a5e63bd934c452e753c90c15e2a95e928e522e6
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2022-04-15 22:04:46 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -365,6 +365,47 @@ def humanbytes(size):
     return str(round(size, 2)) + " " + Dic_powerN[n] + 'B'
 
 
+
+async def gen_thumb(thumbnail, title, userid, ctitle):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(thumbnail) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(f"search/thumb{userid}.png", mode="wb")
+                await f.write(await resp.read())
+                await f.close()
+    image1 = Image.open(f"search/thumb{userid}.png")
+    image2 = Image.open(f"plugins/choose/rrc.png")
+    image3 = changeImageSize(1280, 720, image1)
+    image4 = changeImageSize(1280, 720, image2)
+    image5 = image3.convert("RGBA")
+    image6 = image4.convert("RGBA")
+    Image.alpha_composite(image5, image6).save(f"search/temp{userid}.png")
+    img = Image.open(f"search/temp{userid}.png")
+    draw = ImageDraw.Draw(img)
+    font = ImageFont.truetype("plugins/choose/Roboto-Light.ttf", 55)
+    font2 = ImageFont.truetype("plugins/choose/finalfont.ttf", 65)
+    draw.text(
+        (20, 630),
+        f"{title[:25]}...",
+        fill="White",
+        stroke_width=1,
+        stroke_fill="black",
+        font=font2,
+    )
+    draw.text(
+        (20, 550),
+        f"Playing on: {ctitle[:15]}...",
+        fill="White",
+        stroke_width=1,
+        stroke_fill="black",
+        font=font,
+    )
+    img.save(f"search/final{userid}.png")
+    os.remove(f"search/temp{userid}.png")
+    os.remove(f"search/thumb{userid}.png")
+    final = f"search/final{userid}.png"
+    return final
+
 async def CHAT_TITLE(ctitle):
     string = ctitle
     font1 = list("𝔄𝔅ℭ𝔇𝔈𝔉𝔊ℌℑ𝔍𝔎𝔏𝔐𝔑𝔒𝔓𝔔ℜ𝔖𝔗𝔘𝔙𝔚𝔛𝔜ℨ")
