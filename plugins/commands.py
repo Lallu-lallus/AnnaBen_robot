3528881e287526ce0c922d4197395cc5853c30cc
Lallu-lallus <89923355+Lallu-lallus@users.noreply.github.com>
2022-04-15 11:47:03 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -46,6 +46,11 @@ async def start(client, message):
             InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
         ]]
         reply_markup = InlineKeyboardMarkup(buttons)
+        await message.reply_chat_action("typing")
+        m=await message.reply_sticker("CAACAgUAAxkBAAPLYhmLeQjtiqPZJEHOFh4KFOjbWzcAAvgDAAKfRIlXnd2oxpsLJeYeBA")
+        await asyncio.sleep(1)
+        await m.delete()
+        await message.reply_chat_action("typing")
         await message.reply_photo(
             photo=random.choice(PICS),
             caption=script.START_TXT.format(message.from_user.mention, temp.U_NAME, temp.B_NAME),
